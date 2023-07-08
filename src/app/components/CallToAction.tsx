7b1c542f2a085f6604466f3d601ced2ea37b415d
Eduardo Duca <eduardotexduca@gmail.com>
2023-07-08 13:30:07 -0300

    feat(layout): responsivity home

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -5,10 +5,10 @@ export function CallToAction() {
   return (
     <section
       id="philantropy"
-      className="relative overflow-hidden bg-gray-900 py-20 sm:py-28"
+      className="relative overflow-hidden w-screen bg-gray-900 py-20 sm:py-28"
     >
       <div className="mb-4 pb-[1.6rem]">
-        <div className="absolute left-20 top-[18rem] -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
+        <div className="absolute left-20 top-1/2 -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
           <CircleBackground color="#fff" className="animate-spin" />
         </div>
         <Container className="relative">
@@ -23,7 +23,7 @@ export function CallToAction() {
               Temos orgulho de compartilhar com você nossos processos, números e
               aprendizados ao longo do caminho.
             </p>
-            <h1 className="lg:text-6xl mt-4 text-4xl font-extrabold text-gray-300 border-b-4 lg:w-auto w-72 border-woovi-500">
+            <h1 className="lg:text-6xl mt-4 text-4xl font-extrabold text-gray-300 border-b-4 lg:w-auto w-56 border-woovi-500">
               R$ 255.640,20
             </h1>
             <p className="mt-4 text-lg text-gray-300">Dados até agora</p>
